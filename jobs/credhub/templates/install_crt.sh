9e51dd05d65d17346eadb030bd8a7503079bfa18
Iryna Shustava <ishustava@pivotal.io>
2016-07-06 15:34:06 -0700

    use variable replacement for credential and private key
    -- tricky for multiline manifest properties
    
    [#118202203] Security engineer should be able to configure SSL for CredHub API
    
    Signed-off-by: Larry Hamel <lhamel@pivotal.io>

--- jobs/credhub/templates/install_crt.sh
+++ jobs/credhub/templates/install_crt.sh
@@ -6,7 +6,6 @@ set -e
 
 export JDK_HOME=/var/vcap/packages/openjdk_1.8.0/jdk
 
-#define the certificate to import
 CERT_FILE=$1
 PRIVATE_KEY_FILE=$2
 
@@ -16,15 +15,14 @@ JDK_KEYSTORE_FILE=$JDK_HOME/jre/lib/security/cacerts
 CREDHUB_KEYSTORE_PATH=/var/vcap/jobs/credhub/config/cacerts.jks
 cp $JDK_KEYSTORE_FILE $CREDHUB_KEYSTORE_PATH
 
-openssl pkcs12 -export -in $CERT_FILE -inkey $PRIVATE_KEY_FILE -out cert.p12 \
-        -name $CERT_ALIAS -password pass:k0*l*s3cur1tyr0ck$
-
-$JDK_HOME/bin/keytool -importcert -file cert.p12 -keystore $TRUST_STORE_FILE \
-        -storepass $KEYSTORE_PASSWORD -noprompt -alias $CERT_ALIAS
+openssl pkcs12 -export -in $CERT_FILE -inkey $PRIVATE_KEY_FILE -out cert.p12 -name $CERT_ALIAS -password pass:k0*l*s3cur1tyr0ck$
 
 $JDK_HOME/bin/keytool -importkeystore \
         -srckeystore cert.p12 -srcstoretype PKCS12 -srcstorepass k0*l*s3cur1tyr0ck$ \
         -deststorepass changeit -destkeypass changeit -destkeystore $CREDHUB_KEYSTORE_PATH \
         -alias $CERT_ALIAS
+
+# remove intermediate files
+rm cert.p12 $CERT_FILE $PRIVATE_KEY_FILE
 
 ## END CERTIFICATE INSTALLATION
