c966c6572f13fd17639911391c2e736351b47ff3
Stu Pollock <spollock@pivotal.io>
2016-07-18 12:17:18 -0700

    Extract the version of the github release from the
    name of the tarball
    
    [#126160547] Update pipeline to use bosh-init deployment for acceptance tests
    
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotal.io>

--- ci/tasks/release-name/execute_task.sh
+++ ci/tasks/release-name/execute_task.sh
@@ -3,10 +3,13 @@
 set -exu
 
 RELEASE_NAME_OUTPUT=$(mktemp -d -t release-name-output)
+FAKE_TARBALLS=$(mktemp -d -t credhub-release-fakes)
+touch $FAKE_TARBALLS/credhub-1+dev.1468862844.tgz
 
 fly \
   -t private \
   execute \
   -c task.yml \
   -i task-repo=../../.. \
+  -i credhub-release-tarball=$FAKE_TARBALLS \
   -o release-name-output=$RELEASE_NAME_OUTPUT
