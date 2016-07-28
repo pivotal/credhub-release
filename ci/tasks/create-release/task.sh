23c7d5b3a5863f7e1c03565fb224cb88b211037d
Iryna Shustava <ishustava@pivotal.io>
2016-07-28 15:55:31 -0700

    ci: change create release task to upload release to bosh

--- ci/tasks/create-release/task.sh
+++ ci/tasks/create-release/task.sh
@@ -2,16 +2,30 @@
 
 set -eux
 
-echo $PWD
+WORKING_DIR=${PWD}
 
-pushd release-repo
-    bosh -n create release --name credhub --force --with-tarball --timestamp-version
+if [ -z "$RELEASE_NAME" ]; then
+  exit 1
+fi
 
-    # create release tarball as well so it can be uploaded to s3
-    cd dev_releases/credhub
-    RELEASE_TARBALL=$(ls -t *.tgz | head -1)
-popd
+if [ -z "$RELEASE_VERSION_FILE" ]; then
+  VERSION_ARG="--timestamp-version"
+else
+  VERSION=$(cat $RELEASE_VERSION_FILE)
+  VERSION_ARG="--version $VERSION"
+fi
 
-cp release-repo/dev_releases/credhub/$RELEASE_TARBALL $OUTPUT_PATH
-expr "$RELEASE_TARBALL" : "credhub-\(.*\)\.tgz" | tee /tmp/version
-cp /tmp/version $OUTPUT_PATH
+cd release-dir
+bosh create release --with-tarball --name $RELEASE_NAME --force $VERSION_ARG
+# upload release to bosh
+bosh -t $BOSH_TARGET upload release
+
+cd dev_releases/$RELEASE_NAME
+RELEASE_TARBALL=$(ls -t *.tgz | head -1)
+cp $RELEASE_TARBALL $WORKING_DIR/$OUTPUT_PATH
+expr "$RELEASE_TARBALL" : "$RELEASE_NAME-\(.*\)\.tgz" | tee $WORKING_DIR/$OUTPUT_PATH/version
+if [ -n "$GITHUB_RELEASE_NAME" ]; then
+  echo $GITHUB_RELEASE_NAME > $WORKING_DIR/$OUTPUT_PATH/github_release_name
+  date +%s > $WORKING_DIR/$OUTPUT_PATH/seconds_since_epoch
+fi
+
