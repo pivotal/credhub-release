82c109d8caec477c3c85203fa30cc3190cd2e46b
Iryna Shustava <ishustava@pivotal.io>
2016-07-28 13:04:06 -0700

    ci: fix bug in create-release task

--- ci/tasks/create-release/task.sh
+++ ci/tasks/create-release/task.sh
@@ -2,12 +2,16 @@
 
 set -eux
 
-cd ../../..
+echo $PWD
 
-bosh -n create release --name credhub --force --with-tarball --timestamp-version
+pushd release-repo
+    bosh -n create release --name credhub --force --with-tarball --timestamp-version
 
-# create release tarball as well so it can be uploaded to s3
-cd dev_releases/credhub
-RELEASE_TARBALL=$(ls -t *.tgz | head -1)
-cp $RELEASE_TARBALL $PWD/$OUTPUT_PATH
-expr "$RELEASE_TARBALL" : "credhub-\(.*\)\.tgz" | tee $PWD/$OUTPUT_PATH/version
+    # create release tarball as well so it can be uploaded to s3
+    cd dev_releases/credhub
+    RELEASE_TARBALL=$(ls -t *.tgz | head -1)
+popd
+
+cp release-repo/dev_releases/credhub/$RELEASE_TARBALL $OUTPUT_PATH
+expr "$RELEASE_TARBALL" : "credhub-\(.*\)\.tgz" | tee /tmp/version
+cp /tmp/version $OUTPUT_PATH
