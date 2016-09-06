0c2931e5bd37f255e611683aa0e1023cd8435a7d
Iryna Shustava <ishustava@pivotal.io>
2016-09-06 14:05:34 -0700

    ci: change references from cm-release to credhub-release
    [#129652569] Rename cm-release to credhub-release

--- ci/tasks/release-name/task.sh
+++ ci/tasks/release-name/task.sh
@@ -2,8 +2,8 @@
 
 set -eux
 
-binary_name="credhub-release-tarball"
+release_name="credhub-release-tarball"
 build_number=$(ls credhub-release-tarball/*.tgz | sed -e "s/.*\.\([0-9]*\)\.tgz/\1/g")
 
-echo ${binary_name} > ${RELEASE_NAME_OUTPUT_PATH}/name
+echo ${release_name} > ${RELEASE_NAME_OUTPUT_PATH}/name
 echo ${build_number} > ${RELEASE_NAME_OUTPUT_PATH}/tag
--- ci/tasks/unit-tests/execute_task.sh
+++ ci/tasks/unit-tests/execute_task.sh
@@ -4,4 +4,4 @@ set -exu
 
 rm -rf $RELEASE_DIR/dev_releases/*
 
-fly -t private execute -c task.yml -i cm-release=../../..
+fly -t private execute -c task.yml -i credhub-release=../../..
