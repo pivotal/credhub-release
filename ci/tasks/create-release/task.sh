473249f3a72e25f4cd35d8ada1767b326566ac4a
Iryna Shustava <ishustava@pivotal.io>
2016-09-06 15:45:55 -0700

    ci: Remove GITHUB_RELEASE_NAME param from create-release task
    - release name and tag are created by release-name task. The name and
    tag files created by this task are not used by the pipeline
    
    [#129652569] Rename cm-release to credhub-release

--- ci/tasks/create-release/task.sh
+++ ci/tasks/create-release/task.sh
@@ -26,8 +26,3 @@ cd dev_releases/$RELEASE_NAME
 RELEASE_TARBALL=$(ls -t *.tgz | head -1)
 cp $RELEASE_TARBALL $WORKING_DIR/$OUTPUT_PATH
 expr "$RELEASE_TARBALL" : "$RELEASE_NAME-\(.*\)\.tgz" | tee $WORKING_DIR/$OUTPUT_PATH/version
-if [ -n "$GITHUB_RELEASE_NAME" ]; then
-  echo $GITHUB_RELEASE_NAME > $WORKING_DIR/$OUTPUT_PATH/github_release_name
-  date +%s > $WORKING_DIR/$OUTPUT_PATH/seconds_since_epoch
-fi
-
