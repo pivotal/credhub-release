2ae394a975a39f435c80cca18a77d0461583ad26
Larry Hamel <lhamel@pivotal.io>
2016-09-22 15:00:47 -0700

    pass in VERSION as a system property, and use it to populate /info
    [#130790673] Fix jar version for credhub
    
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- ci/tasks/create-release/execute_task.sh
+++ ci/tasks/create-release/execute_task.sh
@@ -2,8 +2,16 @@
 
 set -exu
 
+temp_dir=$(mktemp -d -t release-name-output)
+
+export OUTPUT_PATH=temp_dir
+export VERSION=sample_version
+export RELEASE_NAME=sample_name
+
 fly \
   -t private \
   execute \
   -c task.yml \
-  -i release-repo=../../.. \
+  -i release-dir=../../.. \
+  -i release-version=../../.. \
+  -o create-release-output=temp_dir
--- ci/tasks/create-release/task.sh
+++ ci/tasks/create-release/task.sh
@@ -9,20 +9,22 @@ if [ -z "$RELEASE_NAME" ]; then
 fi
 
 if [ -z "$RELEASE_VERSION_FILE" ]; then
-  VERSION_ARG="--timestamp-version"
+  TIMESTAMP_OR_VERSION="--timestamp-version"
 else
-  VERSION=$(cat $RELEASE_VERSION_FILE)
-  VERSION_ARG="--version $VERSION"
+  export VERSION=$(cat $RELEASE_VERSION_FILE)
+  TIMESTAMP_OR_VERSION="--version $VERSION"
 fi
 
-cd release-dir
-bosh create release --with-tarball --name $RELEASE_NAME --force $VERSION_ARG
-# upload release to bosh
-bosh -t $BOSH_TARGET login $BOSH_USERNAME $BOSH_PASSWORD
-bosh target $BOSH_TARGET
-bosh upload release
+pushd release-dir
+    bosh create release --with-tarball --name $RELEASE_NAME --force $TIMESTAMP_OR_VERSION
+    # upload release to bosh
+    bosh -t $BOSH_TARGET login $BOSH_USERNAME $BOSH_PASSWORD
+    bosh target $BOSH_TARGET
+    bosh upload release
 
-cd dev_releases/$RELEASE_NAME
-RELEASE_TARBALL=$(ls -t *.tgz | head -1)
-cp $RELEASE_TARBALL $WORKING_DIR/$OUTPUT_PATH
-expr "$RELEASE_TARBALL" : "$RELEASE_NAME-\(.*\)\.tgz" | tee $WORKING_DIR/$OUTPUT_PATH/version
+    pushd dev_releases/$RELEASE_NAME
+        RELEASE_TARBALL=$(ls -t *.tgz | head -1)
+        cp $RELEASE_TARBALL $WORKING_DIR/$OUTPUT_PATH
+        expr "$RELEASE_TARBALL" : "$RELEASE_NAME-\(.*\)\.tgz" | tee $WORKING_DIR/$OUTPUT_PATH/version
+    popd
+popd
