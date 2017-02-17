4a65abee5ed9fa3fbbdd50843068cc27b845f811
Andrew Cantino <acantino@pivotal.io>
2017-02-16 17:41:04 -0800

    Update to openjdk-1.8.0_121
    Add scripts/fetch_latest_java.rb which fetches the latest OpenJDK
    version from the java buildpack team's builds, downloads its JRE,
    updates the openjdk_1.8.0 spec with the new file, and drops the JRE URL
    into the packages/credhub/pre_packaging script for use on CI when
    compiling our code.
    
    [#138453633] Update to latest OpenJDK for CVEs
    
    Signed-off-by: Sebastian Wittenkamp <swittenkamp@pivotal.io>

--- jobs/credhub/templates/install_crt.sh
+++ jobs/credhub/templates/install_crt.sh
@@ -4,7 +4,7 @@ set -e
 
 ## BEGIN CERTIFICATE INSTALLATION
 
-export JDK_HOME=/var/vcap/packages/openjdk_1.8.0/jdk
+export JAVA_HOME=/var/vcap/packages/openjdk_1.8.0/jre
 
 CERT_FILE=$1
 PRIVATE_KEY_FILE=$2
@@ -12,20 +12,20 @@ DATABASE_TLS_CA_FILE=$3
 KEYSTORE_PASSWORD=$4
 
 CERT_ALIAS=credhub_tls_cert
-JDK_KEYSTORE_FILE=$JDK_HOME/jre/lib/security/cacerts
+JDK_KEYSTORE_FILE=$JAVA_HOME/lib/security/cacerts
 CREDHUB_KEYSTORE_PATH=/var/vcap/jobs/credhub/config/cacerts.jks
 CREDHUB_DB_TRUST_STORE_PATH=/var/vcap/jobs/credhub/config/db_trust_store.jks
 DATABASE_VERIFY_CA_ALIAS=database_verify_ca
 
 cp $JDK_KEYSTORE_FILE $CREDHUB_KEYSTORE_PATH
 
-$JDK_HOME/bin/keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $CREDHUB_KEYSTORE_PATH -storepass changeit
+$JAVA_HOME/bin/keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $CREDHUB_KEYSTORE_PATH -storepass changeit
 
 if [ -s $CERT_FILE ]; then
     RANDFILE=/etc/sv/monit/.rnd openssl pkcs12 -export -in $CERT_FILE -inkey $PRIVATE_KEY_FILE -out cert.p12 -name $CERT_ALIAS \
             -password pass:k0*l*s3cur1tyr0ck$
 
-    $JDK_HOME/bin/keytool -importkeystore \
+    $JAVA_HOME/bin/keytool -importkeystore \
             -srckeystore cert.p12 -srcstoretype PKCS12 -srcstorepass k0*l*s3cur1tyr0ck$ \
             -deststorepass $KEYSTORE_PASSWORD -destkeypass $KEYSTORE_PASSWORD -destkeystore $CREDHUB_KEYSTORE_PATH \
             -alias $CERT_ALIAS
@@ -34,9 +34,9 @@ fi
 if [ -s $DATABASE_TLS_CA_FILE ]; then
     cp $JDK_KEYSTORE_FILE $CREDHUB_DB_TRUST_STORE_PATH
 
-    $JDK_HOME/bin/keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $CREDHUB_DB_TRUST_STORE_PATH -storepass changeit
+    $JAVA_HOME/bin/keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $CREDHUB_DB_TRUST_STORE_PATH -storepass changeit
 
-    $JDK_HOME/bin/keytool -import -noprompt -keystore $CREDHUB_DB_TRUST_STORE_PATH -storepass $KEYSTORE_PASSWORD \
+    $JAVA_HOME/bin/keytool -import -noprompt -keystore $CREDHUB_DB_TRUST_STORE_PATH -storepass $KEYSTORE_PASSWORD \
             -alias $DATABASE_VERIFY_CA_ALIAS -file $DATABASE_TLS_CA_FILE
 fi
 
---
+++ scripts/fetch_latest_java.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'open-uri'
+require 'fileutils'
+
+def latest_for(yaml_url, package_name)
+  yaml = YAML.load(open(yaml_url).read)
+  yaml = yaml.inject({}) do |memo, (key, value)|
+    memo[Gem::Version.new(key.gsub("_", "."))] = value
+    memo
+  end
+
+  sorted_versions = yaml.keys.sort
+
+  yaml[sorted_versions.last].tap do |latest|
+    puts "Guessed latest #{package_name}: #{latest} (please confirm!)"
+  end
+end
+
+def latest_jre
+  latest_for 'http://download.pivotal.io.s3.amazonaws.com/openjdk/trusty/x86_64/index.yml', 'jre'
+end
+
+def latest_jdk
+  latest_for 'https://java-buildpack.cloudfoundry.org/openjdk-jdk/trusty/x86_64/index.yml', 'jdk'
+end
+
+jre_url = latest_jre
+jdk_url = latest_jdk
+jre_filename = File.basename(URI(jre_url).path)
+
+# Write JDK URL to the packages/credhub/pre_packaging script. We need the JDK to build our code on CI.
+pre_packaging_script_path = 'packages/credhub/pre_packaging'
+pre_packaging_script = File.read(pre_packaging_script_path)
+File.open(pre_packaging_script_path, 'w') do |f|
+  f.print pre_packaging_script.sub(/JDK_URL="[^"]+"/, "JDK_URL=\"#{jdk_url}\"")
+end
+
+# Download current JRE
+FileUtils.mkdir_p("blobs/openjdk_1.8.0")
+system "cd blobs/openjdk_1.8.0 && curl \"#{jre_url}\" -o \"#{jre_filename}\""
+
+# Update blobs/openjdk_1.8.0/spec
+spec_path = 'packages/openjdk_1.8.0/spec'
+spec = YAML.load_file(spec_path)
+spec['files'][0] = "openjdk_1.8.0/#{jre_filename}"
+File.open(spec_path, 'w') { |f| f.puts YAML.dump(spec) }
+
+puts "\nIf this looks good, do a dev release for testing, and finally \"bosh upload blobs\" and commit when you're happy."
