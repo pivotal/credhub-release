9af2d6995b12d77cf189e739ae9111d52ec28e12
Joe Kirwin <jkirwin@pivotal.io>
2017-02-22 17:17:29 -0800

    Create separate keystores for server and db certs
    [#139869879] Make sure we're not accepting any CA cert when talking to the DB
    
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- jobs/credhub/templates/install_crt.sh
+++ jobs/credhub/templates/install_crt.sh
@@ -12,14 +12,9 @@ DATABASE_TLS_CA_FILE=$3
 KEYSTORE_PASSWORD=$4
 
 CERT_ALIAS=credhub_tls_cert
-JDK_KEYSTORE_FILE=$JAVA_HOME/lib/security/cacerts
 CREDHUB_KEYSTORE_PATH=/var/vcap/jobs/credhub/config/cacerts.jks
 CREDHUB_DB_TRUST_STORE_PATH=/var/vcap/jobs/credhub/config/db_trust_store.jks
 DATABASE_VERIFY_CA_ALIAS=database_verify_ca
-
-cp $JDK_KEYSTORE_FILE $CREDHUB_KEYSTORE_PATH
-
-$JAVA_HOME/bin/keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $CREDHUB_KEYSTORE_PATH -storepass changeit
 
 if [ -s $CERT_FILE ]; then
     RANDFILE=/etc/sv/monit/.rnd openssl pkcs12 -export -in $CERT_FILE -inkey $PRIVATE_KEY_FILE -out cert.p12 -name $CERT_ALIAS \
@@ -32,10 +27,6 @@ if [ -s $CERT_FILE ]; then
 fi
 
 if [ -s $DATABASE_TLS_CA_FILE ]; then
-    cp $JDK_KEYSTORE_FILE $CREDHUB_DB_TRUST_STORE_PATH
-
-    $JAVA_HOME/bin/keytool -storepasswd -new $KEYSTORE_PASSWORD -keystore $CREDHUB_DB_TRUST_STORE_PATH -storepass changeit
-
     $JAVA_HOME/bin/keytool -import -noprompt -keystore $CREDHUB_DB_TRUST_STORE_PATH -storepass $KEYSTORE_PASSWORD \
             -alias $DATABASE_VERIFY_CA_ALIAS -file $DATABASE_TLS_CA_FILE
 fi
