5afa7d19fe6e65d0987f09dc2fa834eaab6784f7
Iryna Shustava <ishustava@pivotal.io>
2017-08-03 17:07:42 -0700

    Verify database CA on backup and restore
    [#149507205] Operators should be able to use BBR to backup and restore non-local postgres database
    
    Signed-off-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- spec/spec/backup_erb_spec.rb
+++ spec/spec/backup_erb_spec.rb
@@ -5,7 +5,7 @@ require 'yaml'
 require 'json'
 require 'fileutils'
 
-def render_backup_erb(dbtype="postgres")
+def render_backup_erb(dbtype="postgres", require_tls, tls_ca)
   option_yaml = <<-EOF
         properties:
           credhub:
@@ -15,6 +15,8 @@ def render_backup_erb(dbtype="postgres")
               host: 127.0.0.1
               port: 5432
               database: example_credhub
+              require_tls: #{require_tls}
+              tls_ca: #{tls_ca}
               type: #{dbtype}
   EOF
 
@@ -26,9 +28,11 @@ end
 RSpec.describe "the template" do
   context "when db is postgres" do
     it "includes the pgdump command" do
-      result = render_backup_erb()
+      result = render_backup_erb(false, nil)
       expect(result).to include('export PGUTILS_DIR=/var/vcap/packages/pg_utils_9.4')
       expect(result).to include('export PGPASSWORD="example_password"')
+      expect(result).to_not include('export PGSSLMODE="verify-full"')
+      expect(result).to_not include('export PGSSLROOTCERT=/var/vcap/jobs/credhub/config/database_ca.pem')
       expect(result).to include '"${PGUTILS_DIR}/bin/pg_dump" \\' + "\n" +
       '  --user="example_username" \\' + "\n" +
       '  --host="127.0.0.1" \\' + "\n" +
@@ -36,10 +40,23 @@ RSpec.describe "the template" do
       '  --format="custom" \\' + "\n" +
       '  "example_credhub" > "${BBR_ARTIFACT_DIRECTORY}/credhubdb_dump"'
     end
+    it "includes the pgdump command and require_tls is true" do
+      result = render_backup_erb(true, "test_tls_ca")
+      expect(result).to include('export PGUTILS_DIR=/var/vcap/packages/pg_utils_9.4')
+      expect(result).to include('export PGPASSWORD="example_password"')
+      expect(result).to include('export PGSSLMODE="verify-full"')
+      expect(result).to include('export PGSSLROOTCERT=/var/vcap/jobs/credhub/config/database_ca.pem')
+      expect(result).to include '"${PGUTILS_DIR}/bin/pg_dump" \\' + "\n" +
+                                    '  --user="example_username" \\' + "\n" +
+                                    '  --host="127.0.0.1" \\' + "\n" +
+                                    '  --port="5432" \\' + "\n" +
+                                    '  --format="custom" \\' + "\n" +
+                                    '  "example_credhub" > "${BBR_ARTIFACT_DIRECTORY}/credhubdb_dump"'
+    end
   end
   context "when db is not postgres" do
     it "logs that it skips this backup," do
-      result = render_backup_erb("NOT_PG")
+      result = render_backup_erb("NOT_PG", nil, nil)
       expect(result).to_not include "/var/vcap/packages/pg_utils_9.4/bin/pg_dump \\\n"
       expect(result).to include 'Skipping backup, as database is not Postgres'
     end
--- spec/spec/restore_erb_spec.rb
+++ spec/spec/restore_erb_spec.rb
@@ -5,7 +5,7 @@ require 'yaml'
 require 'json'
 require 'fileutils'
 
-def render_restore_erb(dbtype="postgres")
+def render_restore_erb(dbtype="postgres", require_tls, tls_ca)
   option_yaml = <<-EOF
         properties:
           credhub:
@@ -15,6 +15,8 @@ def render_restore_erb(dbtype="postgres")
               host: 127.0.0.1
               port: 5432
               database: example_credhub
+              require_tls: #{require_tls}
+              tls_ca: #{tls_ca}
               type: #{dbtype}
   EOF
 
@@ -25,10 +27,12 @@ end
 
 RSpec.describe "the template" do
   context "when db is postgres" do
-    it "includes the pgrestore command" do
-      result = render_restore_erb()
+    it "includes the pgrestore command with require_tls:false" do
+      result = render_restore_erb(false, nil)
       expect(result).to include('export PGUTILS_DIR=/var/vcap/packages/pg_utils_9.4')
       expect(result).to include('export PGPASSWORD="example_password"')
+      expect(result).to_not include('export PGSSLMODE="verify-full"')
+      expect(result).to_not include('export PGSSLROOTCERT=/var/vcap/jobs/credhub/config/database_ca.pem')
       expect(result).to include '"${PGUTILS_DIR}/bin/pg_restore" \\' + "\n" +
       '  --user="example_username" \\' + "\n" +
       '  --host="127.0.0.1" \\' + "\n" +
@@ -38,10 +42,25 @@ RSpec.describe "the template" do
       '  --clean \\' + "\n" +
       '  --dbname="example_credhub" "${BBR_ARTIFACT_DIRECTORY}/credhubdb_dump"'
     end
+    it "includes the pgrestore command with ssl properties when require_tls:true" do
+      result = render_restore_erb(true, "test_tls_ca")
+      expect(result).to include('export PGUTILS_DIR=/var/vcap/packages/pg_utils_9.4')
+      expect(result).to include('export PGPASSWORD="example_password"')
+      expect(result).to include('export PGSSLMODE="verify-full"')
+      expect(result).to include('export PGSSLROOTCERT=/var/vcap/jobs/credhub/config/database_ca.pem')
+      expect(result).to include '"${PGUTILS_DIR}/bin/pg_restore" \\' + "\n" +
+                                    '  --user="example_username" \\' + "\n" +
+                                    '  --host="127.0.0.1" \\' + "\n" +
+                                    '  --port="5432" \\' + "\n" +
+                                    '  --format="custom" \\' + "\n" +
+                                    '  --schema="public" \\' + "\n" +
+                                    '  --clean \\' + "\n" +
+                                    '  --dbname="example_credhub" "${BBR_ARTIFACT_DIRECTORY}/credhubdb_dump"'
+    end
   end
   context "when db is not postgres" do
     it "logs that it skips this restore," do
-      result = render_restore_erb("NOT_PG")
+      result = render_restore_erb("NOT_PG", nil, nil)
       expect(result).to_not include "${PGUTILS_DIR}/bin/pg_dump \\\n"
       expect(result).to include 'Skipping restore, as database is not Postgres'
     end
