4a3a61abd29fd2d120ae9f931dc2171c64274cbc
Ben Moss <bmoss@pivotal.io>
2017-11-08 17:20:54 -0500

    Run pre-start in post-restore-unlock
    [#152599772] bbr deployment backup not restarting CredHub successfully
    
    Signed-off-by: Danny Sullivan <dsullivan@pivotal.io>

--- jobs/credhub/templates/post-restore-unlock.sh
+++ jobs/credhub/templates/post-restore-unlock.sh
@@ -3,5 +3,7 @@
 set -u
 
 export PATH=/var/vcap/bosh/bin:/var/vcap/jobs/credhub/bin:$PATH
+
+exec /var/vcap/jobs/credhub/bin/pre-start
 monit start credhub
 exec /var/vcap/jobs/credhub/bin/post-start
