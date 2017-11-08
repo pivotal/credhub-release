aec80e1d5d0b3d2c77a88d9e0d10b6067d00dbc0
Danny Sullivan <dsullivan@pivotal.io>
2017-11-08 17:52:57 -0500

    Call monit restart from post-bbr-unlock
    [#152599772] bbr deployment backup not restarting CredHub successfully
    
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

--- jobs/credhub/templates/post-restore-unlock.sh
+++ jobs/credhub/templates/post-restore-unlock.sh
@@ -4,6 +4,5 @@ set -u
 
 export PATH=/var/vcap/bosh/bin:/var/vcap/jobs/credhub/bin:$PATH
 
-exec /var/vcap/jobs/credhub/bin/pre-start
-monit start credhub
+monit restart credhub
 exec /var/vcap/jobs/credhub/bin/post-start
