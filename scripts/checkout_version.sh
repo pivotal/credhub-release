e014ded7888b78537a1162882248d5bad2d54a4b
Marty Spiewak <mspiewak@pivotal.io>
2019-05-21 09:03:23 -0400

    Add script to checkout version of credhub

---
+++ scripts/checkout_version.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function set_bash_error_handling() {
+    set -euo pipefail
+}
+
+function go_to_project_root_directory() {
+    local -r script_dir=$( dirname "${BASH_SOURCE[0]}")
+
+    cd "$script_dir/.."
+}
+
+function checkout_credhub() {
+    declare -r version=$1
+    declare -r release__sha="$(bosh interpolate --path /commit_hash \
+    <(git show "origin/master:releases/credhub/credhub-$version.yml"))"
+
+    git co "$release__sha"
+    git su
+}
+
+function main() {
+    set_bash_error_handling
+    go_to_project_root_directory
+    checkout_credhub "$1"
+}
+
+main "$@"
