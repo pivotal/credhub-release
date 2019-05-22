3e3176fae23a36f8289df246e20152b500679a5c
Marty Spiewak <mspiewak@pivotal.io>
2019-05-22 14:16:57 -0400

    add script to fix ruby linting errors
    Co-authored-by: Josh Zarrabi <jzarrabi@pivotal.io>

---
+++ scripts/fix_ruby_linting_errors.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function set_bash_error_handling() {
+    set -euo pipefail
+}
+
+function go_to_project_root_directory() {
+    local -r script_dir=$( dirname "${BASH_SOURCE[0]}")
+
+    cd "$script_dir/.."
+}
+
+function install_gems() {
+    pushd spec >/dev/null
+        bundle
+    popd >/dev/null
+}
+
+function lint_ruby() {
+    pushd spec >/dev/null
+        bundle exec rubocop -a
+    popd >/dev/null
+}
+
+function main() {
+    set_bash_error_handling
+    go_to_project_root_directory
+
+    install_gems
+    lint_ruby
+}
+
+main "$@"
