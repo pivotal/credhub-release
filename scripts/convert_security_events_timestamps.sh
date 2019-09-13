9f78b8f553d69eb238b8fae78e072d75c5f44a5f
Brian Vullo <bvullo@pivotal.io>
2019-09-13 14:21:44 -0400

    update script to convert timestamps ue UTC and millis
    [#168138208]

--- scripts/convert_security_events_timestamps.sh
+++ scripts/convert_security_events_timestamps.sh
@@ -3,9 +3,12 @@
 newFile=${2:-"/tmp/credhub_security_events_with_timestamps.log"}
 touch "$newFile"
 
+export TZ=UTC
+
 while IFS= read -r line; do
-    timestamp="$(echo "$line" | awk '{print $3}' | cut -d "=" -f2 | xargs date -r)"
-    echo "$line" | sed -E "s/rt=[0-9]*/rt=\"${timestamp}\"/" >> "$newFile"
+    unixTimestamp="$(echo "$line" | awk '{print $3}' | cut -d "=" -f2)"
+    dateTime="$(date -r $((unixTimestamp/1000)))"
+    echo "$line" | sed -E "s/rt=[0-9]*/rt=\"${dateTime}\"/" >> "$newFile"
 done < "$1"
 
 echo "Converted file saved at $newFile"
